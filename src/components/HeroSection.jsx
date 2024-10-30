01edacc1bfd8c17d4dde190224e6b6d3d6748cec
Thomaz Szeckir <thomazszeckir@Thomazs-Air.lan>
2024-10-29 21:39:26 -0400

    :bug: button HeroSection and hover added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,10 +7,10 @@ function HeroSection() {
             <div className="flex-row text-4xl italic font-bold">
                 <p className='mb-1'>Made</p>
                 <p className='mb-1'>Exclusively</p>
-                <p className='mb-8'>for Men</p>
+                <p className='mb-8'>for Men.</p>
 
-                <div>
-                    <a href='https://booksy.com/en-us/184757_boogies-barbershop_barber-shop_134763_orlando#ba_s=seo' target='_blank' className="px-14 py-1 bg-gradient-to-r from-orange-600 to-orange-900 font-bold rounded w-full">
+                <div className=''>
+                    <a href='https://booksy.com/en-us/184757_boogies-barbershop_barber-shop_134763_orlando#ba_s=seo' target='_blank' className="p-3 lg:px-14 py-1 bg-gradient-to-r from-orange-600 to-orange-900 text-transparent bg-clip-text border border-neutral-700 font-bold rounded w-full hover:text-orange-900">
                         Book Now
                     </a>
                 </div>
